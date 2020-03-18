c509d368819c0dae4ddfe2cf0fb8b8f62fa6b31a
张红亮 <zhanghongliang@192.168.1.105>
2020-03-18 20:22:55 +0800

    psychanalysis

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import * as mathematics from './metric'
 import { runRender } from './physics'
 import { r } from './neuroscience'
+import { init, animate } from './psychanalysis'
 new mathematics.Metric()
 runRender()
 console.log(r())
+init();
+animate();
