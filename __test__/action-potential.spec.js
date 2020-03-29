4e3a724e8aeda1df0a75eced518b342ecb4aa4e7
张红亮 <zhanghongliang@192.168.1.102>
2020-03-29 20:13:36 +0800

    cycle

--- __test__/action-potential.spec.js
+++ __test__/action-potential.spec.js
@@ -1,4 +1,7 @@
 const { ActionPotential } = require('../neuron/action-potential')
+
 it(` If the voltage changes by a large enough amount over a short interval, the neuron generates an all-or-nothing electrochemical pulse called an action potential.`, () => {
-    expect(new ActionPotential().ini.SodiumInterval).toBe(.5)
+    const ins = new ActionPotential();
+    expect(ins.ini.sodiumInterval).toBe(.5)
+    expect(ins.getCycleInterval()).toBeGreaterThan(.5).toBeLessThan(100)
 });
--- neuron/action-potential.js
+++ neuron/action-potential.js
@@ -2,8 +2,14 @@
 class ActionPotential {
     // If the voltage changes by a large enough amount over a short interval, the neuron generates an all-or-nothing electrochemical pulse called an action potential.
     ini = {
-        SodiumInterval: .5,//Sodium-based action potentials usually last for under one millisecond, but calcium-based action potentials may last for 100 milliseconds or longer.
-        calciumInterval: 100
+        sodiumInterval: .5,//Sodium-based action potentials usually last for under one millisecond, but calcium-based action potentials may last for 100 milliseconds or longer.
+        calciumInterval: 100,
+        // A typical voltage across an animal cell membrane is −70 mV.
+        typicalVoltage: -70
+
+    }
+    getCycleInterval() {
+        return this.ini.sodiumInterval + Math.random() * (this.ini.calciumInterval - this.ini.sodiumInterval)
     }
 }
 module.exports = { ActionPotential }
