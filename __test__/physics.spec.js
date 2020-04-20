f12d70950a1e1ea58f5450b75522404fb64209c5
Philipp <zhanghongliang@192.168.1.105>
2020-04-21 03:07:36 +0800

    harmonic motion

--- __test__/physics.spec.js
+++ __test__/physics.spec.js
@@ -1,6 +1,13 @@
-const { RainDrop } = require('../physics/power')
+const { RainDrop, HarmonicMotion } = require('../physics/power')
+it(``, () => {
+    const harmonic = new HarmonicMotion()
+    expect(harmonic.getPeriod()).toBeCloseTo(5067, 1)
+    expect(harmonic.getMinute()).toBeCloseTo(84, 0)
+});
 it(``, () => {
     const rainDrop = new RainDrop({})
     expect(rainDrop.getHumanTerminal()).toBeCloseTo(70, -.3)//Math.pow(10,-(-.3))/2 ≈ 1, the precision ≈ 1
     expect(rainDrop.getCatTerminal()).toBeCloseTo(40, -.3)
+    expect(rainDrop.getInsectTerminal()).toBeCloseTo(9, -.3)
+    expect(rainDrop.getRainTerminal()).toBeCloseTo(4, -.3)
 });
--- physics/power.js
+++ physics/power.js
@@ -1,5 +1,19 @@
 const math = require('mathjs')
 require('../physicalConstants')
+class HarmonicMotion {
+    constructor() { }
+    getPeriod() {
+        const T = '2*π*sqrt(3/(4*π*ρ*G))'
+        return math.evaluate(T, {
+            π: math.pi,
+            ρ: 5.5e3,
+            G: math.gravitationConstant.value
+        })
+    }
+    getMinute() {
+        return this.getPeriod() / 60
+    }
+}
 class RainDrop {
     constructor(config) {
         const defaults = {
@@ -9,6 +23,13 @@ class RainDrop {
     }
     human = .6
     cat = .2
+    insect = .01
+    rain = .002
+    rainShape = {
+        cloudDroplet: 1e-5,
+        smallDrop: 1e-4,
+        bigDrop: [1e-3, 6 * 1e-3]
+    }
     // 假设没有任何外力施加或所施加的外力之和为零，则运动中物体总保持匀速直线运动状态，静止物体总保持静止状态
     getMotionState() {
         const gravity = this.getGravity()
@@ -27,6 +48,12 @@ class RainDrop {
     getCatTerminal() {
         return this.getTerminalVelocity(this.cat)
     }
+    getInsectTerminal() {
+        return this.getTerminalVelocity(this.insect)
+    }
+    getRainTerminal() {
+        return this.getTerminalVelocity(this.rain)
+    }
     getTerminalVelocity(d) {
         // return math.evaluate('sqrt(2*m*g/ρ*Cd*A)',{
         return math.evaluate('90*sqrt(d)', {
@@ -41,4 +68,4 @@ class RainDrop {
         return +(this.m * math.gravity.value)
     }
 }
-module.exports = { RainDrop }
+module.exports = { RainDrop, HarmonicMotion }
