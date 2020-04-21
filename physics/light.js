0e5090a463ade0296aa64755e714dee159e08022
Philipp <zhanghongliang@192.168.1.107>
2020-04-21 16:38:35 +0800

    ratio

--- physics/light.js
+++ physics/light.js
@@ -11,11 +11,23 @@ class Light {
         Object.assign(this, defaults, config)
     }
     waterRefractiveIndex = 1.33
+    getSinRatio() {
+        return math.sin(this.angle) / math.sin(this.getRadians())
+    }
+    getSpeedRation() {
+        return this.velocity / (this.getSpeed() * 1000)
+    }
     getSpeed() {
         return math.round(this.velocity / this.waterRefractiveIndex / 1000)
     }
+    getSpeedWithUnit() {
+        return this.getSpeed() + 'km/s'
+    }
     toDegrees(angle) {
         return angle * (180 / Math.PI);
+    }
+    getRadians() {
+        return math.asin(math.sin(this.angle) / this.waterRefractiveIndex)
     }
     getAngle() {
         return this.toDegrees(math.asin(math.sin(this.angle) / this.waterRefractiveIndex))
