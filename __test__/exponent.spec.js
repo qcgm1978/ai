1dc63f29036dbf84710b57c01c353b7e35beb8a4
Philipp <zhanghongliang@192.168.1.106>
2020-05-02 16:50:36 +0800

    range

--- __test__/exponent.spec.js
+++ __test__/exponent.spec.js
@@ -2,8 +2,15 @@ const { Exponent, Logarithm } = require('../mathematics/calculus/exponent')
 const exponent = new Exponent()
 const log = new Logarithm()
 it(``, () => {
+    expect(log.inRange({ expression: 'lga(3)', isMoreThanOne: false })).toEqual([-Infinity, 0]);
+    expect(log.inRange({ expression: 'lga(.33)', isMoreThanOne: false })).toEqual([0, Infinity]);
+    expect(log.inRange({ expression: 'a^3', isMoreThanOne: false })).toEqual([0, 1]);
+    expect(log.inRange({ expression: 'a^-3', isMoreThanOne: true })).toEqual([0, 1]);
+});
+it(``, () => {
     expect(log.inequation('log3(x)>-1')).toBe("x > 0.33")
     expect(log.solveInEquations({ equations: ['log(3)/log(x)>0.5'], variable: 'x' })).toBe("1 < x < 9")
+    expect(log.solveEquations({ equations: ['0=log(y)/log(0.3)', 'y=x^2-3*x'], variable: 'x' })).toBeCloseTo(-.303)
 });
 it(``, () => {
     expect(exponent.isInDomain(5)).toBeTruthy()
--- mathematics/calculus/exponent.js
+++ mathematics/calculus/exponent.js
@@ -14,6 +14,26 @@ class Logarithm extends CalculusSet {
     }
     isInDomain = this.isPositiveR
     isInCodomain = this.isR
+    inRange({ expression, isMoreThanOne }) {
+        if (/lg/.test(expression)) {
+
+            const [express, base, antilog] = expression.match(/lg(.*)\((.*)\)/)
+            if (antilog < 1) {
+                return isMoreThanOne ? [-Infinity, 0] : [0, Infinity]
+            } else {
+
+                return isMoreThanOne ? [0, Infinity] : [-Infinity, 0]
+            }
+        } else {
+            const [express, base, log] = expression.match(/(.+)\^(.+)/)
+            if (log > 0) {
+                return isMoreThanOne ? [1, Infinity] : [0, 1]
+            } else {
+                return isMoreThanOne ? [0, 1] : [1, Infinity]
+
+            }
+        }
+    }
     inequation(expression) {
         const [, base, power, sign, lg] = expression.match(/log(.+)\((.+)\)(.)(-?.+)/)
         const decimal = (base ** lg).toFixed(2)
